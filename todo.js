3bd44a3534d4ed4304f8e9682760e05c68bd0456
oosakiken1 <9690408@gmail.com>
2021-11-26 20:58:21 +0900

    Add files via upload

--- todo.js
+++ todo.js
@@ -6,28 +6,29 @@ const taskStatus = document.getElementById('taskstatus');
 const taskTitle = document.getElementById('tasktitle');
 const taskDetail = document.getElementById('taskdetail');
 const submitButton = document.getElementById('submit');
-const saveButton = document.getElementById('save');
-const loadButton = document.getElementById('load');
-
-// サンプル追加
-addTask('2021-07', '済', 'A社経営統合プロジェクト', '経営統合に伴う業務プロセス統合プロジェクト。\nプロジェクトリーダー（メンバー４人）として担当。\nＱＤＣ目標いずれも達成。ＣＳ評価で５をいただいた。');
 
 // 登録ボタンイベント
 submitButton.onclick = () => {
-  addTask(taskMonth.value, taskStatus.value, taskTitle.value, taskDetail.value)
+  const task = {
+    month: taskMonth.value,
+    status: taskStatus.value,
+    title: taskTitle.value,
+    detail: taskDetail.value
+  }
+  addTask(task);
 }
 
 // 登録処理
-function addTask(month, status, title, detail) {
+function addTask(task) {
 
   //１行追加
   const row = taskTable.insertRow(-1);
 
   //５列追加
-  row.insertCell(-1).innerText = month;
-  row.insertCell(-1).innerText = status;
-  row.insertCell(-1).innerText = title;
-  row.insertCell(-1).innerText = detail;
+  row.insertCell(-1).innerText = task.month;
+  row.insertCell(-1).innerText = task.status;
+  row.insertCell(-1).innerText = task.title;
+  row.insertCell(-1).innerText = task.detail;
 
   //削除ボタン①
   row.insertCell(-1).innerHTML = '<button onclick="deleteTask(this)">削除</button>';
@@ -37,12 +38,16 @@ function addTask(month, status, title, detail) {
   // deleteButton.onclick = deleteTask2;
   // deleteButton.innerText = "削除";
   // row.insertCell(-1).appendChild(deleteButton);
+
+  Save(); //結果を保存
 }
 
 //削除処理
 function deleteTask(obj) {
   const tr = obj.parentNode.parentNode;           // <button>.<td>.<tr>
   tr.parentNode.deleteRow(tr.sectionRowIndex);    // <tr>.<table>.deleteRow()
+
+  Save(); //結果を保存
 }
 
 // function deleteTask2() {
@@ -50,18 +55,26 @@ function deleteTask(obj) {
 //   tr.parentNode.deleteRow(tr.sectionRowIndex);
 // }
 
-//保存ボタンイベント
-saveButton.onclick = () => {
-  if (confirm('データを保存しますか')) {
-    localStorage.setItem('tasktable', taskTable.innerHTML);
-    alert('保存が完了しました')
+//保存処理
+function Save() {
+  localStorage.setItem('tasktable', taskTable.innerHTML);
+}
+
+//読込処理
+function load() {
+  const taskTableHTML = localStorage.getItem('tasktable');
+  if (!taskTableHTML) {
+    // localStorageが空のときは、サンプル追加
+    const task = {
+      month: '2021-07',
+      status: '済',
+      title: 'A社経営統合プロジェクト',
+      detail: '経営統合に伴う業務プロセス統合プロジェクト。\nプロジェクトリーダー（メンバー４人）として担当。\nＱＤＣ目標いずれも達成。ＣＳ評価で５をいただいた。'
+    }
+    addTask(task);
+  } else {
+    taskTable.innerHTML = taskTableHTML;
   }
 }
 
-//読込ボタンイベント
-loadButton.onclick = () => {
-  if (confirm('データを読み込みますか')) {
-    taskTable.innerHTML = localStorage.getItem('tasktable');
-  }
-}
-
+load();
